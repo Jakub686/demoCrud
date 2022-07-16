f75f168a2657d68e332f00644a32112ea2b004f5
Jakub <jakub.bchyla@gmail.com>
2022-07-16 10:54:14 +0200

    Initial commit

---
+++ src/main/java/com/example/demoCrud/MovieController.java
@@ -0,0 +1,26 @@
+package com.example.demoCrud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @GetMapping("/movies")
+    public List<Movie> getAll(){
+        return movieRepository.getAll();
+    }
+
+    @GetMapping("/movies/{id}")
+    public Movie getById(@PathVariable("id") int id){
+        return movieRepository.getById(id);
+    }
+}
